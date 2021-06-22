86e14fcb96792b39692ed0c5d20c6e888ffa0e16
Joshua Levy <joshua.levy@capco.com>
2021-06-22 15:51:08 +0100

    Missing lines from previous commit Problem: Previous commit dealing with failure was not done correctly

--- front-end/flutter_confluence/lib/core/usecases/usecase.dart
+++ front-end/flutter_confluence/lib/core/usecases/usecase.dart
@@ -1,3 +1,6 @@
-abstract class UseCase<Type> {
-  Future<Type> call();
+import 'package:dartz/dartz.dart';
+import 'package:flutter_confluence/core/errors/failures.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
 }
--- front-end/flutter_confluence/lib/domain/entities/cloud_certification.dart
+++ front-end/flutter_confluence/lib/domain/entities/cloud_certification.dart
@@ -1,16 +1,42 @@
-class CloudCertification {
+import 'package:equatable/equatable.dart';
+
+import '../../core/constants.dart';
+
+class CloudCertification extends Equatable{
   final String name;
   final String platform;
   final String certificationType;
   final String certificationDate;
+  String? certificationIconName;
 
-  CloudCertification({
-    required this.name,
-    required this.platform,
-    required this.certificationType,
-    required this.certificationDate,
-  });
+  CloudCertification(
+      {required this.name,
+      required this.platform,
+      required this.certificationType,
+      required this.certificationDate}){
+    setCertificationIconName();
+  }
 
-  List<Object> get props =>
-      [name, platform, certificationType, certificationDate];
+  void setCertificationIconName() {
+    switch (this.certificationType) {
+      case Constants.GCP:
+        certificationIconName = Constants.IC_GCP;
+        break;
+      case Constants.AZURE:
+        certificationIconName = Constants.IC_AZURE;
+        break;
+      case Constants.CLOUD_NATIVE:
+        certificationIconName = Constants.IC_CLOUD_NATIVE;
+        break;
+      case Constants.HASHICORP:
+        certificationIconName = Constants.IC_HASHICORP;
+        break;
+      case Constants.AWS:
+        certificationIconName = Constants.IC_AWS;
+
+    }
+  }
+
+  @override
+  List<Object?> get props => [name, platform, certificationType, certificationType, certificationIconName];
 }
