63e60f77e0fe134f659989d011fbf5389536d2a8
hmxs-capco <haris.mexis@capco.com>
2021-06-23 18:38:29 +0100

    generated mocks with mockito

--- front-end/flutter_confluence/test/presentation/bloc/cloud_certification_bloc_test.dart
+++ front-end/flutter_confluence/test/presentation/bloc/cloud_certification_bloc_test.dart
@@ -1,28 +1,55 @@
+import 'package:flutter_confluence/core/constants.dart';
 import 'package:flutter_confluence/domain/usecases/get_completed_certification.dart';
 import 'package:flutter_confluence/domain/usecases/get_in_progress_certification.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-
+import 'package:dartz/dartz.dart';
 import 'package:flutter_confluence/presentation/bloc/cloud_certification_bloc.dart';
+import 'cloud_certification_bloc_test.mocks.dart';
 
-class MockGetCompleted extends Mock implements GetCompletedCertification {}
-
-class MockGetInProgress extends Mock implements GetInProgressCertification {}
-
+@GenerateMocks([GetCompletedCertification, GetInProgressCertification])
 void main() {
-  late MockGetCompleted mockCompletedCase = MockGetCompleted();
-  late MockGetInProgress mockInProgressCase = MockGetInProgress();
   late CloudCertificationBloc bloc;
+  late MockGetCompletedCertification mockCompletedCase = MockGetCompletedCertification();
+  late MockGetInProgressCertification mockInProgressCase = MockGetInProgressCertification();
 
   setUp(() {
-    mockCompletedCase = MockGetCompleted();
-    mockInProgressCase = MockGetInProgress();
     bloc = CloudCertificationBloc(
         completedUseCase: mockCompletedCase,
         inProgressUseCase: mockInProgressCase);
   });
 
-  test('initialState should be Empty', () {
+  test('initial bloc state should be Empty', () {
     expect(bloc.state, equals(Empty()));
   });
+
+  test(
+    'should get list of completed certifications use case',
+        () async {
+      // arrange
+      when(mockCompletedCase.execute(any))
+          .thenAnswer((_) async => Right(mockCompletedCerts));
+      // act
+      bloc.add(GetCompletedCertificationsEvent());
+      await untilCalled(mockCompletedCase.execute(any));
+      // assert
+      verify(mockCompletedCase.execute(NoParams()));
+    },
+  );
+
+  test(
+    'should get list of in progress certifications use case',
+        () async {
+      // arrange
+      when(mockInProgressCase.execute(any))
+          .thenAnswer((_) async => Right(mockInrogressCerts));
+      // act
+      bloc.add(GetInProgressCertificationsEvent());
+      await untilCalled(mockInProgressCase.execute(any));
+      // assert
+      verify(mockInProgressCase.execute(NoParams()));
+    },
+  );
+
 }
