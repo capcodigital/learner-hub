f6b9fb692bfc92cf68594a46d15ed8d2c4bdf961
Joshua Levy <joshua.levy@capco.com>
2021-06-23 13:51:18 +0100

    Remove Usecase callable file Problem: The usecases file that allows them to be callable is redundant

--- front-end/flutter_confluence/lib/core/usecases/usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter_confluence/core/errors/failures.dart';
-
-abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> Execute(Params params);
-}
--- front-end/flutter_confluence/lib/domain/usecases/get_in_progress_certification.dart
+++ front-end/flutter_confluence/lib/domain/usecases/get_in_progress_certification.dart
@@ -2,22 +2,20 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../core/errors/failures.dart';
-import '../../core/usecases/usecase.dart';
 import '../entities/cloud_certification.dart';
 import '../repositories/cloud_certification_repository.dart';
 
-class GetInProgressCertification
-    implements UseCase<CloudCertification, NoParams> {
+class GetInProgressCertification {
   final CloudCertificationRepository repository;
 
   GetInProgressCertification(this.repository);
 
-  @override
-  Future<Either<Failure, CloudCertification>> Execute(NoParams noParams) async {
+  Future<Either<Failure, CloudCertification>> execute(NoParams noParams) async {
     return await repository.getInProgressCertifications();
   }
 }
 
+@override
 class NoParams extends Equatable {
   List<Object> get props => [];
 }
