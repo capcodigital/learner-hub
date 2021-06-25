61994a28db09861d0b90b9bd2038f8151a5e6697
hmxs-capco <haris.mexis@capco.com>
2021-06-25 12:09:24 +0100

    moved constants to related file

--- front-end/flutter_confluence/lib/presentation/bloc/cloud_certification_bloc.dart
+++ front-end/flutter_confluence/lib/presentation/bloc/cloud_certification_bloc.dart
@@ -4,17 +4,13 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import '../../core/error/failures.dart';
 import '../../core/usecases/usecase.dart';
+import '../../core/constants.dart';
 import '../../domain/entities/cloud_certification.dart';
 import '../../domain/usecases/get_completed_certifications.dart';
 import '../../domain/usecases/get_in_progress_certifications.dart';
 
 part 'cloud_certification_event.dart';
 part 'cloud_certification_state.dart';
-
-// Move error messages to constants file
-const SERVER_FAILURE_MSG = "Server Failure";
-const CACHE_FAILURE_MSG = "Cache Failure";
-const UNKNOWN_ERROR_MSG = "Unknown Error";
 
 class CloudCertificationBloc
     extends Bloc<CloudCertificationEvent, CloudCertificationState> {
@@ -51,11 +47,11 @@ class CloudCertificationBloc
   String _mapFailureToMessage(Failure failure) {
     switch (failure.runtimeType) {
       case ServerFailure:
-        return SERVER_FAILURE_MSG;
+        return Constants.SERVER_FAILURE_MSG;
       case CacheFailure:
-        return CACHE_FAILURE_MSG;
+        return Constants.CACHE_FAILURE_MSG;
       default:
-        return UNKNOWN_ERROR_MSG;
+        return Constants.UNKNOWN_ERROR_MSG;
     }
   }
 }
