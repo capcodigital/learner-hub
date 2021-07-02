0397005a720380b268b3b4d58b0aa026570dc618
hmxs-capco <haris.mexis@capco.com>
2021-07-02 17:47:10 +0100

    working on unit tests

--- front-end/flutter_confluence/test/features/on_boarding/presentation/bloc/on_boarding_bloc_test.dart
+++ front-end/flutter_confluence/test/features/on_boarding/presentation/bloc/on_boarding_bloc_test.dart
@@ -1,8 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_confluence/core/usecases/usecase.dart';
 import 'package:flutter_confluence/features/onboarding/domain/usecases/authenticate_use_case.dart';
 import 'package:flutter_confluence/features/onboarding/domain/usecases/check_cached_auth_use_case.dart';
 import 'package:flutter_confluence/features/onboarding/presentation/bloc/on_boarding_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'on_boarding_bloc_test.mocks.dart';
 
@@ -28,14 +31,30 @@ void main() {
 
   group('', () {
     test(
-      '',
+      'Should call the Authentication use case',
       () async {
         // arrange
-
+        when(mockAuthenticateCase(any))
+            .thenAnswer((_) async => Right(true));
         // act
-
+        bloc.add((AuthenticationEvent()));
+        await untilCalled(mockAuthenticateCase(any));
         // assert
+        verify(mockAuthenticateCase(NoParams()));
+      },
+    );
 
+    test(
+      'Should call the CheckCachedAuth use case',
+          () async {
+        // arrange
+        when(mockCheckCachedAuthCase(any))
+            .thenAnswer((_) async => Right(true));
+        // act
+        bloc.add((CheckCachedAuthEvent()));
+        await untilCalled(mockCheckCachedAuthCase(any));
+        // assert
+        verify(mockCheckCachedAuthCase(NoParams()));
       },
     );
 
