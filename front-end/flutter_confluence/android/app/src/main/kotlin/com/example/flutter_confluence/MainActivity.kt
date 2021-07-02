421d8e241266e27b4aa6db60ef121c4177c5319d
hmxs-capco <haris.mexis@capco.com>
2021-07-02 14:07:44 +0100

    added required android configurations for local_auth

--- front-end/flutter_confluence/android/app/src/main/kotlin/com/example/flutter_confluence/MainActivity.kt
+++ front-end/flutter_confluence/android/app/src/main/kotlin/com/example/flutter_confluence/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.flutter_confluence
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
