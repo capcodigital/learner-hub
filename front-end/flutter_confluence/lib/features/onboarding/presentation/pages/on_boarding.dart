015213bd48893e1435e26a35b1524975cf589b3f
hmxs-capco <haris.mexis@capco.com>
2021-07-15 10:46:37 +0100

    added tests

--- front-end/flutter_confluence/lib/features/onboarding/presentation/pages/on_boarding.dart
+++ front-end/flutter_confluence/lib/features/onboarding/presentation/pages/on_boarding.dart
@@ -20,10 +20,10 @@ class OnBoardingPage extends StatelessWidget with CustomAlertDialog  {
   static const msgAuthenticate = "Authenticate";
   static const msgAuthenticateNotSupported = "Continue";
 
-  static const platformIconRadius = 26.0;
+  static const platformIconRadius = 20.0;
   static const card_radius = 15.0;
   static const cardWidth = 180.0;
-  static const cardHeight = 260.0;
+  static const cardHeight = 220.0;
 
   void authenticate(BuildContext context) {
     BlocProvider.of<OnBoardingBloc>(context).add(AuthEvent());
@@ -63,10 +63,14 @@ class OnBoardingPage extends StatelessWidget with CustomAlertDialog  {
               top: Dimen.bgFrontLayerTop,
               child: Image.asset('assets/front-layer.png'),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: Dimen.dimen_80),
+            Container(
+              margin: const EdgeInsets.only(top: 60), //const EdgeInsets.only(top: Dimen.dimen_80),
               child: buildBody(context),
             )
+            // Padding(
+            //   padding: const EdgeInsets.only(top: 10), //const EdgeInsets.only(top: Dimen.dimen_80),
+            //   child: buildBody(context),
+            // )
           ],
         ));
   }
@@ -97,11 +101,12 @@ class OnBoardingPage extends StatelessWidget with CustomAlertDialog  {
           alignment: Alignment.center,
           child: Text(
             msgDescription,
+            maxLines: 2,
             style: Theme.of(context).textTheme.headline2,
             textAlign: TextAlign.center,
           ),
           margin: EdgeInsets.only(
-              top: Dimen.dimen_34, right: Dimen.dimen_68, left: Dimen.dimen_68),
+              top: 10, right: 40, left: 40),
         ),
         buildAuthButton(context)
       ],
@@ -141,7 +146,9 @@ class OnBoardingPage extends StatelessWidget with CustomAlertDialog  {
             style: Theme.of(context)
                 .textTheme
                 .headline2
-                ?.copyWith(color: Colors.white)),
+                ?.copyWith(color: Colors.white,
+               fontSize: 12
+            )),
         Container(
           margin: EdgeInsets.only(top: Dimen.dimen_6),
           child: Column(
@@ -187,7 +194,9 @@ class OnBoardingPage extends StatelessWidget with CustomAlertDialog  {
         style: Theme.of(context)
             .textTheme
             .headline1
-            ?.copyWith(color: Colors.white),
+            ?.copyWith(color: Colors.white,
+            fontSize: 12
+        ),
       ),
     );
   }
---
+++ front-end/flutter_confluence/test/features/on_boarding/presentation/pages/on_boarding_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:flutter_confluence/features/onboarding/presentation/bloc/on_boarding_bloc.dart';
+import 'package:flutter_confluence/features/onboarding/presentation/pages/on_boarding.dart';
+
+import 'on_boarding_test.mocks.dart';
+
+class UnknownState extends OnBoardingState {}
+
+@GenerateMocks([OnBoardingBloc])
+void main() {
+  const NUM_OF_CARDS = 2;
+  const NUM_OF_CIRCLE_AVATARS = 6;
+  const NUM_OF_IMAGES = 4;
+
+  MockOnBoardingBloc mockBloc = MockOnBoardingBloc();
+
+  setMockBlockState(OnBoardingState state) {
+    when(mockBloc.state).thenAnswer((_) => state);
+    when(mockBloc.stream).thenAnswer((_) => Stream.value(state));
+  }
+
+  testWidgets('OnBoarding Page should show expected widgets',
+      (WidgetTester tester) async {
+    // arrange
+    setMockBlockState(Empty());
+
+    // act
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<OnBoardingBloc>(
+            create: (_) => mockBloc, child: OnBoardingPage()),
+      ),
+    );
+
+    final cardFinder = find.byWidgetPredicate((widget) => widget is Card);
+    final circleAvatarFinder =
+        find.byWidgetPredicate((widget) => widget is CircleAvatar);
+    final imgFinder = find.byWidgetPredicate((widget) => widget is Image);
+    final btnFinder =
+        find.byWidgetPredicate((widget) => widget is ElevatedButton);
+
+    // assert
+    expect(cardFinder, findsNWidgets(NUM_OF_CARDS));
+    expect(circleAvatarFinder, findsNWidgets(NUM_OF_CIRCLE_AVATARS));
+    expect(imgFinder, findsNWidgets(NUM_OF_IMAGES));
+    expect(btnFinder, findsOneWidget);
+  });
+}
