a113e53df485c8a44b218559700239911e3b247e
Camilo Galiana <camilo.galiana@capco.com>
2021-08-12 17:03:42 +0100

    [BENCH-643] Added more auth middleware unit tests

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -2,8 +2,16 @@ import request from 'supertest';
 import { app } from './../src/index';
 
 describe("Post Endpoints", () => {
-    it("should fetch a single post", async () => {
-        const res = await request(app).get(`/hello`);
+    it("should return 401 error if not token pass", async () => {
+        const res = await request(app)
+            .get(`/hello`);
+        expect(res.statusCode).toEqual(401);
+    });
+
+    it("should return 401 error if token is not valid", async () => {
+        const res = await request(app)
+            .get(`/hello`)
+            .set('Authorization', 'Bearer invalidFakeToken');
         expect(res.statusCode).toEqual(401);
     });
 });
