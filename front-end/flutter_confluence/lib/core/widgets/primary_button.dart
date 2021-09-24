a6ebd2be8a7060c53a9a68acee14f52817350ebf
Camilo Galiana <camilo.galiana@capco.com>
2021-09-24 16:49:28 +0100

    [BENCH-801] Fix analysis errors

--- front-end/flutter_confluence/lib/core/widgets/primary_button.dart
+++ front-end/flutter_confluence/lib/core/widgets/primary_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_confluence/core/Colours.dart';
-
+import '../colours.dart';
 import '../dimen.dart';
 
 class PrimaryButton extends StatelessWidget {
