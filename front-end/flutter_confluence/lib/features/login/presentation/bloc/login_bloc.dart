fc4345047553d352205b3895626755146f76c7db
Camilo Galiana <camilo.galiana@capco.com>
2021-09-27 15:47:38 +0100

    [BENCH-803] Fixed analysis error

--- front-end/flutter_confluence/lib/features/login/presentation/bloc/login_bloc.dart
+++ front-end/flutter_confluence/lib/features/login/presentation/bloc/login_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import '/core/auth/auth_failures.dart';
 import '/core/error/failures.dart';
 import '/features/login/domain/usecases/login_use_case.dart';
