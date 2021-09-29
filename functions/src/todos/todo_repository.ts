3b574bc144a679d1b055de4290a0338b7d9c854b
Haris Mexis <83984426+hmxs-capco@users.noreply.github.com>
2021-09-29 16:48:50 +0100

    Feat - Bench 789 - TODOs endpoint (#122)
    * updated User model
    
    * updated code for signup User
    
    * updating user
    
    * small fix
    
    * working on user certifications
    
    * getting uid from auth
    
    * working on user certifications
    
    * updates
    
    * rename
    
    * using firebase/auth to get current user
    
    * get current user
    
    * returning updated user in response
    
    * code format
    
    * cleanup
    
    * added await where required
    
    * removed unneeded methods
    
    * cleanup
    
    * updates
    
    * save usercertification and return item
    
    * updates
    
    * working on /get certifications
    
    * added all endpoints
    
    * removed unneeded dependencies
    
    * updates
    
    * checking if user exists in users table before call repo to get their certifications
    
    * small fixes
    
    * small fix
    
    * sending 404 if certif not found on /update, /delete
    
    * returning 403 Forbiden when user tries to edit/delete certification of other userid
    
    * cleanup
    
    * added todos endpoints
    
    * updates
    
    * method rename
    
    * udpates
    
    * corrections
    
    * correction
    
    * correction
    
    * pr corrections
    
    * pr corrections
    
    * updated api docs
    
    * pr corrections
    
    * updated api docs
    
    * updated api docs
    
    * updated api docs
    
    * pr corrections

---
+++ functions/src/todos/todo_repository.ts
@@ -0,0 +1,81 @@
+/* eslint-disable require-jsdoc */
+import * as admin from 'firebase-admin';
+
+const TABLE_TODOS = "TODOs";
+
+export class TODOFirestoreError extends Error { }
+export class TODOExistsError extends TODOFirestoreError { }
+export class TODONotFoundError extends TODOFirestoreError { }
+export class AccessForbidenError extends TODOFirestoreError { }
+
+export async function getUserTODOs(uid: string): Promise<any[]> {
+    const collection = admin.firestore().collection(TABLE_TODOS);
+    const snap = await collection.where("userId", "==", uid).get();
+    return toTODOs(snap);
+}
+
+export async function insert(todo: TODO) {
+    const col = admin.firestore().collection(TABLE_TODOS);
+    const docRef = await col.add({ todo });
+    return {
+        id: docRef.id,
+        userId: todo.userId,
+        title: todo.title,
+        content: todo.content,
+        isCompleted: todo.isCompleted
+    }
+}
+
+export async function update(
+    todoId: string,
+    todo: TODO
+) {
+    const doc = admin.firestore().collection(TABLE_TODOS).doc(todoId);
+    const docRef = await doc.get();
+    if (docRef.exists) {
+        const item = docRef.data() as TODO;
+        if (todo.userId == item.userId) {
+            doc.update(todo);
+            const updated = docRef.data() as TODO;
+            return {
+                id: docRef.id,
+                userId: updated.userId,
+                title: updated.title,
+                content: updated.content,
+                isCompleted: updated.isCompleted
+            }
+        } else throw new AccessForbidenError("TODO is not of current user");
+    } else throw new TODONotFoundError();
+}
+
+// "delete" is a reserved word
+export async function deleteItem(
+    uid: string,
+    todoId: string
+) {
+    const doc = admin.firestore().collection(TABLE_TODOS).doc(todoId);
+    const docRef = await doc.get();
+    if (docRef.exists) {
+        const item = docRef.data() as TODO;
+        if (uid == item.userId) doc.delete();
+        else throw new AccessForbidenError("TODO is not of current user");
+    }
+    else throw new TODONotFoundError();
+}
+
+async function toTODOs(snap: FirebaseFirestore.QuerySnapshot): Promise<any[]> {
+    const items = Array<any>();
+    if (!snap.empty) {
+        snap.forEach(docRef => {
+            const it = docRef.data() as TODO;
+            items.push({
+                id: docRef.id,
+                userId: it.userId,
+                title: it.title,
+                content: it.content,
+                isCompleted: it.isCompleted
+            });
+        });
+    }
+    return items;
+}
