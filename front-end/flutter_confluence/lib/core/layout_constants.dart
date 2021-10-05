3eb2948053816c87e5bcac5908f012412c09dadd
hmxs-capco <haris.mexis@capco.com>
2021-10-05 15:32:57 +0100

    deleted unneeded dimensions

--- front-end/flutter_confluence/lib/core/layout_constants.dart
+++ front-end/flutter_confluence/lib/core/layout_constants.dart
@@ -1,7 +1,5 @@
 class LayoutConstants {
   // New
-  static const dimen_62 =
-      62.0; // TODO(cgal-capco): Rename to something that makes more sense than this
   static const button_widget_height = 62.0;
 
   static const extra_small_padding = 8.0;
@@ -11,28 +9,13 @@ class LayoutConstants {
   static const extra_large_padding = 64.0;
 
   // Legacy
-  static const dimen_6 = 6.0;
-  static const dimen_8 = 8.0;
-  static const dimen_10 = 10.0;
-  static const dimen_14 = 14.0;
-  static const dimen_16 = 16.0;
-  static const dimen_20 = 20.0;
-  static const dimen_26 = 26.0;
   static const dimen_32 = 32.0;
-  static const dimen_34 = 34.0;
-  static const dimen_40 = 40.0;
   static const dimen_48 = 48.0;
-  static const dimen_60 = 60.0;
-  static const dimen_68 = 68.0;
-  static const dimen_74 = 74.0;
-  static const dimen_80 = 80.0;
-  static const dimen_100 = 100.0;
 
   static const mainBtnHeight = 60.0;
   static const mainBtnWidth = 230.0;
   static const mainBtnBorderRadius = 20.0;
   static const mainBtnBorderWidth = 1.0;
-
   static const bgFrontLayerLeft = 88.0;
   static const bgFrontLayerTop = 270.0;
 
@@ -40,38 +23,19 @@ class LayoutConstants {
   static const scale_2_100 = 0.02;
   static const scale_4_100 = 0.04;
   static const scale_6_100 = 0.06;
-  static const scale_7_100 = 0.07;
-  static const scale_8_100 = 0.08;
   static const scale_9_100 = 0.09;
   static const scale_5_100 = 0.05;
   static const scale_12_100 = 0.12;
   static const scale_13_100 = 0.13;
-  static const scale_14_100 = 0.14;
-  static const scale_15_100 = 0.15;
-  static const scale_22_100 = 0.22;
-  static const scale_24_100 = 0.24;
-  static const scale_25_100 = 0.25;
-  static const scale_28_100 = 0.28;
-  static const scale_43_100 = 0.43;
-  static const scale_10_100 = 0.1;
   static const scale_20_100 = 0.2;
-  static const scale_30_100 = 0.3;
   static const scale_35_100 = 0.35;
   static const scale_40_100 = 0.4;
   static const scale_50_100 = 0.5;
-  static const scale_64_100 = 0.64;
   static const scale_70_100 = 0.7;
   static const scale_80_100 = 0.8;
   static const scale_90_100 = 0.9;
   static const scale_95_100 = 0.95;
-
-  static const scale_6_1000 = 0.006;
-  static const scale_2_1000 = 0.002;
-  static const scale_22_1000 = 0.022;
   static const scale_25_1000 = 0.025;
-  static const scale_35_1000 = 0.035;
-  static const scale_43_1000 = 0.022;
-  static const scale_48_1000 = 0.048;
   static const scale_56_1000 = 0.056;
 
   static const ratio_40_55 = 4 / 5.5;
