d866277ff17770cc8abece34a77e51a8fe54359f
hmxs-capco <haris.mexis@capco.com>
2021-10-06 12:17:36 +0100

    making dimension mulitple of 8

--- front-end/flutter_confluence/lib/core/layout_constants.dart
+++ front-end/flutter_confluence/lib/core/layout_constants.dart
@@ -1,20 +1,22 @@
 class LayoutConstants {
 
+  static const BASE = 8.0;
+
   static const EXTRA_SMALL_PADDING = 8.0;
   static const SMALL_PADDING = 16.0;
   static const REGULAR_PADDING = 24.0;
   static const LARGE_PADDING = 32.0;
   static const EXTRA_LARGE_PADDING = 64.0;
 
-  static const BTN_WIDGET_HEIGHT = 62.0;
-  static const APP_DRAWER_LOGO_LEFT_PADDING = 32.0;
-  static const APP_DRAWER_LOGO_VERTICAL_PADDING = 48.0;
-  static const MAIN_BTN_HEIGHT = 60.0;
-  static const MAIN_BTN_WIDTH = 230.0;
-  static const MAIN_BTN_BORDER_RADIUS = 20.0;
+  static const BTN_WIDGET_HEIGHT = 8 * BASE;
+  static const APP_DRAWER_LOGO_LEFT_PADDING = 4 * BASE;
+  static const APP_DRAWER_LOGO_VERTICAL_PADDING = 6 * BASE;
+  static const MAIN_BTN_HEIGHT = 8 * BASE;
+  static const MAIN_BTN_WIDTH = 29 * BASE;
+  static const MAIN_BTN_BORDER_RADIUS = 2.5 * BASE;
   static const MAIN_BTN_BORDER_WIDTH = 1.0;
-  static const BG_FRONT_LAYER_LEFT = 88.0;
-  static const BG_FRONT_LAYER_TOP = 270.0;
+  static const BG_FRONT_LAYER_LEFT = 11 * BASE;
+  static const BG_FRONT_LAYER_TOP = 34 * BASE;
 
   static const TINY_SCALE = 0.02;
   static const EXTRA_SMALL_SCALE = 0.12;
