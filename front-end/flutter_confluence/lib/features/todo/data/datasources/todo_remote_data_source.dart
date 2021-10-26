8f5c5fa2e47666a4019cd5f7c90df1305e9a67aa
Andreas Velounias <andreas.velounias@capco.com>
2021-10-26 18:02:57 +0100

    Add datasource implementations

--- front-end/flutter_confluence/lib/features/todo/data/datasources/todo_remote_data_source.dart
+++ front-end/flutter_confluence/lib/features/todo/data/datasources/todo_remote_data_source.dart
@@ -1,34 +1,106 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_confluence/core/constants.dart';
+import 'package:flutter_confluence/core/error/custom_exceptions.dart';
 import 'package:flutter_confluence/features/todo/data/models/todo_model.dart';
-import 'package:flutter_confluence/features/todo/domain/entities/todo.dart';
+import 'package:http/http.dart' as http;
 
 abstract class TodoRemoteDataSource {
   Future<List<TodoModel>> getTodos();
-  Future<TodoModel> addTodo();
-  Future<TodoModel> updateTodo();
-  Future<void> deleteTodo();
+  Future<TodoModel> addTodo(TodoModel todo);
+  Future<TodoModel> updateTodo(TodoModel todo);
+  Future<void> deleteTodo(TodoModel todo);
 }
 
 class TodoRemoteDataSourceImpl implements TodoRemoteDataSource {
+  TodoRemoteDataSourceImpl({required this.auth, required this.client});
+
+  final FirebaseAuth auth;
+  final http.Client client;
   @override
-  Future<TodoModel> addTodo() {
-    // TODO: implement addTodo
-    throw UnimplementedError();
+  Future<TodoModel> addTodo(TodoModel todo) async {
+    try {
+      const url = '${Constants.BASE_API_URL}/todos';
+
+      final token = await auth.currentUser?.getIdToken();
+
+      final body = jsonEncode(todo);
+
+      final response = await client.post(Uri.parse(url),
+          headers: {
+            HttpHeaders.authorizationHeader: 'Bearer $token',
+            HttpHeaders.contentTypeHeader: 'application/json'
+          },
+          body: body);
+      if (response.statusCode == 200) {
+        final results = json.decode(response.body)['data'];
+        final todo = TodoModel.fromJson(results);
+        return todo;
+      } else {
+        throw ServerException(message: 'Internal Server Error');
+      }
+    } on Exception catch (ex) {
+      throw ServerException(message: ex.toString());
+    }
   }
 
   @override
-  Future<void> deleteTodo() {
-    // TODO: implement deleteTodo
-    throw UnimplementedError();
+  Future<void> deleteTodo(TodoModel todo) async {
+    try {
+      final url = '${Constants.BASE_API_URL}/todos/${todo.id}';
+
+      final token = await auth.currentUser?.getIdToken();
+
+      final response = await client.delete(
+        Uri.parse(url),
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+          HttpHeaders.contentTypeHeader: 'application/json'
+        },
+      );
+      if (response.statusCode == 200) {
+        return;
+      } else {
+        throw ServerException(message: 'Internal Server Error');
+      }
+    } on Exception catch (ex) {
+      throw ServerException(message: ex.toString());
+    }
   }
 
   @override
-  Future<List<TodoModel>> getTodos() {
-    // TODO: implement getTodos
-    throw UnimplementedError();
+  Future<List<TodoModel>> getTodos() async {
+    try {
+      const url = '${Constants.BASE_API_URL}/todos';
+
+      final token = await auth.currentUser?.getIdToken();
+
+      final response = await client.get(
+        Uri.parse(url),
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer $token',
+          HttpHeaders.contentTypeHeader: 'application/json'
+        },
+      );
+      if (response.statusCode == 200) {
+        final results = json.decode(response.body)['data'];
+        final todos =
+            (results as List).map((e) => TodoModel.fromJson(e)).toList();
+        return todos;
+      } else {
+        throw ServerException(message: 'Internal Server Error');
+      }
+    } on Exception catch (ex) {
+      throw ServerException(message: ex.toString());
+    }
   }
 
   @override
-  Future<TodoModel> updateTodo() {
+  Future<TodoModel> updateTodo(TodoModel todo) {
     // TODO: implement updateTodo
     throw UnimplementedError();
   }
