66d3c9e1e1c9c1d23d589792bb49f286cb111d0c
Camilo Galiana <camilo.galiana@capco.com>
2022-02-07 10:10:07 +0000

    [BENCH-995] Update Firebase Function dependencies - Fixes security warnings

--- functions/src/custom.d.ts
+++ functions/src/custom.d.ts
@@ -1,10 +1,10 @@
-import { auth } from "firebase-admin/lib/auth"
+import { DecodedIdToken } from "firebase-admin/auth";
 
 declare global {
-    // Extend the Request type from Express Framework to add the user
-    namespace Express {
-        export interface Request {
-            user?: auth.DecodedIdToken
-        }
+  // Extend the Request type from Express Framework to add the user
+  namespace Express {
+    export interface Request {
+      user?: DecodedIdToken;
     }
+  }
 }
